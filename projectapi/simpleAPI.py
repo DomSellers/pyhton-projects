ae2b3a95d2e9f3cecf9138b8d38f089be9657272
Dominic Sellers <domsellers41@gmail.com>
2025-11-26 21:29:59 +0000

    add chat loop and update ask_groq() to use dynamic user prompts

--- projectapi/simpleAPI.py
+++ projectapi/simpleAPI.py
@@ -2,15 +2,31 @@ from groq import Groq
 
 client = Groq(api_key="")
 
-
+messages = []
 def ask_groq(prompt):
+    messages.append({"role": "user", "content": prompt})
+
+    
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=[
-            {"role": "user", "content": "Hello! Who are you?"}
+            {"role": "user", "content": prompt} # edit: make the AI respond to a prompt
         ]
     )
 
     return response.choices[0].message.content
 
-print(ask_groq("Hello! Who are you?"))
+print(ask_groq("Ask me how I am and initate the conversation"))  #edit: edit print statement to initiate the upcoming conversation loop by asking user how they are. 
+
+#edit: adding loop to be able to talk to the chatbot
+while True:
+    user_input = input("💬: ") #creating a variable to store user inputs for the prompt
+
+    if user_input.lower() == "exit": #if statement to break loop if user types exit"
+        print("Goodbye!")
+        break
+
+
+    reply = ask_groq(user_input)
+    print("AI:", reply,"\n")
+    
